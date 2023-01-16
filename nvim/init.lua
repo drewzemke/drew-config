a13c0c38d047971b5f31169212d4e2b784b5f5a4
Drew Zemke <drew.zemke@gmail.com>
2023-01-16 14:56:35 -0800

    added options config

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,11 @@ vim.g.mapleader = ' '
 
 -- [[ config ]]
 require('config.plugins')
+require('config.options')
+
+vim.api.nvim_create_autocmd( 'User', {
+	pattern = 'VeryLazy',
+	callback = function()
+		require('config.keymaps')
+	end,
+})
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,30 @@
+--[[ opts.lua ]]
+
+local opt = vim.opt
+
+-- [[ Context ]]
+opt.number = true		        -- Line numbers
+opt.relativenumber = true 	-- Relative line numbers
+opt.scrolloff = 4	        	-- Min num of lines of context
+opt.signcolumn = "yes"	  	-- Show the sign column ?? 
+
+-- [[ Filetypes ]]
+opt.encoding = 'utf8'		    -- String encoding
+opt.fileencoding = 'utf8'	  -- File encoding
+
+-- [[ Search ]]
+opt.ignorecase = true		    -- Ignore case in search
+opt.smartcase = true		    -- Override ignorecase if search has capital letters
+opt.incsearch = true		    -- Incremental search
+opt.hlsearch = true		      -- Highlight search
+
+-- [[ Whitespace ]]
+opt.expandtab = true		    -- Spaces instead of tabs
+opt.shiftwidth = 2		      -- Indent size
+opt.softtabstop	= 2		      -- Number of spaces tabs count as (in insert mode)
+opt.tabstop = 2			        -- Number of spaces tabs count as ??
+
+-- [[ Splits ]]
+opt.splitright = true		    -- Place new window to the right of current
+opt.splitbelow = true		    -- Place new window below current
+
