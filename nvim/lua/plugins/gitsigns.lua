47c5f7c6af5be5ba203093770ce9079e6164ecbe
Drew Zemke <drew.zemke@gmail.com>
2023-01-17 11:28:07 -0800

    plugin: added gitsigns

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,24 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "BufReadPre",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  opts = {
+    current_line_blame_formatter = "<author>:<author_time:%Y-%m-%d> - <summary>",
+    signs                        = {
+      add          = { text = "▌" },
+      change       = { text = "▌" },
+      topdelete    = { text = "▔" },
+      delete       = { text = "▁" },
+      changedelete = { text = "▁" },
+      untracked    = { text = "▌" }
+    },
+    current_line_blame_opts      = {
+      virt_text         = true,
+      virt_text_pos     = "right_align",
+      delay             = 10,
+      ignore_whitespace = false,
+    },
+  }
+}
