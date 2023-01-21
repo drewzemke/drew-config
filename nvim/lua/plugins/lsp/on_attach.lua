4a111b1b2abdd844a88fd9f8c99a8c92ea57b0aa
Drew Zemke <drew.zemke@gmail.com>
2023-01-21 13:22:28 -0800

    lsp: setup autocommand for on-save formatting

--- nvim/lua/plugins/lsp/on_attach.lua
+++ nvim/lua/plugins/lsp/on_attach.lua
@@ -1,4 +1,4 @@
-return function (client, bufnr)
+return function(client, bufnr)
   -- Setup lsp signature
   if client.supports_method('textDocument/signatureHelp') then
     require('lsp_signature').on_attach({
@@ -11,19 +11,29 @@ return function (client, bufnr)
   if client.supports_method('textDocument/formatting') then
     vim.api.nvim_buf_set_var(bufnr, 'format_with_lsp', true)
   end
-  
+
+  -- Create autocommand for on-save formatting
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = vim.api.nvim_create_augroup('Format', { clear = true }),
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.formatting_seq_sync()
+      end
+    })
+  end
+
   -- Keymaps
   local telescope = require('telescope.builtin')
   local opts = { noremap = true, silent = true, buffer = bufnr }
   local keymaps = {
     normal = {
-      ['R'] = {vim.lsp.buf.rename, opts},
-      ['gr'] = {telescope.lsp_references, opts}
+      ['R'] = { vim.lsp.buf.rename, opts },
+      ['gr'] = { telescope.lsp_references, opts },
+      ['gd'] = { telescope.lsp_definitions, opts }
       -- ADD MORE
     }
   }
-
-  -- TODO setup autocmds? whatever those are...
 
   -- Setup keymaps with util
   require('utils.keymap-loader').load(keymaps)
