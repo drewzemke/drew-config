9e0f33636b2ab7eb358e2e6df20f22521934b033
Drew Zemke <drew.zemke@gmail.com>
2023-01-21 13:18:45 -0800

    plugins: added mason with minimal config

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+  'williamboman/mason.nvim',
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+  },
+
+  config = function ()
+    require('mason').setup();
+
+    local server_names = vim.tbl_keys(require('plugins.lsp.servers'))
+    
+
+    require('mason-lspconfig').setup({
+      ensure_installed = server_names,
+--      automatic_installation = { exclude = server_names }
+    })
+  end
+}
