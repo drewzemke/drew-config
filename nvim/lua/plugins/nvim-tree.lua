52318032ecb3167a407ae20dd4d81289d7afcc31
Drew Zemke <drew.zemke@gmail.com>
2023-01-21 22:39:43 -0800

    plugin(nvim-tree): setup highlighting based on git status

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -5,7 +5,30 @@ return {
     'nvim-tree/nvim-web-devicons'
   },
 
+
   config = function()
-    require('nvim-tree').setup({})
+    require('nvim-tree').setup({
+      modified = {
+        enable = true,
+      },
+
+      renderer = {
+        icons = {
+          show = {
+            git = true,
+            modified = true,
+          },
+          git_placement = 'after',
+        },
+
+        indent_markers = {
+          enable = true,
+        },
+
+        highlight_git = true,
+        highlight_modified = 'name',
+      }
+    })
+
   end
 }
